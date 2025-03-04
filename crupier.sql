761f37d2077d9925820c02046a553814481f583b
Guillermo Aliaga Fernández <guillermo.aliaga@estudiantat.upc.edu>
2025-03-04 23:21:48 +0100

    Database2

--- crupier.sql
+++ crupier.sql
@@ -3,8 +3,8 @@ CREATE DATABASE crupier;
 USE crupier;
 CREATE TABLE actcrupier(
 ID_crup integer primary key not null,
-engaño FLOAT,
+engano FLOAT,
 trampa FLOAT
 );
-INSERT INTO actcrupier values (1,23.1,58.3);
-INSERT INTO actcrupier values (2,79.8,5);
+INSERT INTO actcrupier(ID_crup,engano,trampa)VALUES(1,23.1,58.3);
+INSERT INTO actcrupier(ID_crup,engano,trampa)VALUES(2,79.8,5);
