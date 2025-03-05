8391e144f7c92d981efb299db20e7d23dd2b5fe7
GuillermoAliaga <127043932+GuillermoAliaga@users.noreply.github.com>
2025-03-05 16:47:25 +0100

    Update crupier.sql

--- crupier.sql
+++ crupier.sql
@@ -2,9 +2,25 @@ DROP DATABASE IF EXISTS crupier;
 CREATE DATABASE crupier;
 USE crupier;
 CREATE TABLE actcrupier(
-ID_crup integer primary key not null,
+ID_crup INTEGER PRIMARY KEY NOT NULL,
 engano FLOAT,
 trampa FLOAT
-);
-INSERT INTO actcrupier(ID_crup,engano,trampa)VALUES(1,23.1,58.3);
-INSERT INTO actcrupier(ID_crup,engano,trampa)VALUES(2,79.8,5);
+)ENGINE = InnoDB;
+CREATE TABLE sala(
+ID_sala INTEGER PRIMARY KEY NOT NULL,
+ID_crup INTEGER PRIMARY KEY NOT NULL,
+numjug INTEGER
+)ENGINE = InnoDB;
+CREATE TABLE t1-t3(
+actcrupier INTEGER NOT NULL,
+sala INTEGER NOT NULL,
+FOREIGN KEY (actcrupier) REFERENCES actcrupier(ID_crup),
+FOREIGN KEY (sala) REFERENCES sala(ID_crup)
+)ENGINE = InnoDB;
+
+INSERT INTO actcrupier VALUES (1,23.1,58.3);
+INSERT INTO actcrupier VALUES (2,79.8,5);
+INSERT INTO sala VALUES (1,3,2);
+INSERT INTO sala VALUES (2,1,5);
+INSERT INTO t1-t3 VALUES (1,1);
+INSERT INTO t1-t3 VALUES (2,2);
